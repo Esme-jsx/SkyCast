352cf1c45d3eb7b530c80dba5b134988833c7823
Esme-jsx <esme.jsx@gmail.com>
2024-10-11 18:02:24 -0400

    Added days and months array in index.js, and added apiKey, apiURL, and temporary city

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let apiKey = "t0fa0a4b6o62052bf581e37227b2ab41";
+let city = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Saturday"];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
